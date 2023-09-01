8260a106792cfcf3bc60b5b8bcb2a28aa4be73fb
RozAle <110930300+RuzannaAleksanyan@users.noreply.github.com>
2023-09-01 17:37:30 +0400

    Update abstract_factory.hpp

--- creational_patterns/abstract_factory/abstract_factory.hpp
+++ creational_patterns/abstract_factory/abstract_factory.hpp
@@ -6,19 +6,19 @@
 class AbstractProductA {
 public:
     virtual ~AbstractProductA() {}
-    virtual std::string getName() const = 0;
+    virtual std::string get_name() const = 0;
 };
 
 class ConcreteProductA1 : public AbstractProductA {
 public:
-    std::string getName() const override {
+    std::string get_name() const override {
         return "ConcreteProductA1";
     }
 };
 
 class ConcreteProductA2 : public AbstractProductA {
 public:
-    std::string getName() const override {
+    std::string get_name() const override {
         return "ConcreteProductA2";
     }
 };
@@ -26,57 +26,57 @@ public:
 class AbstractProductB {
 public:
     virtual ~AbstractProductB() {}
-    virtual std::string getName() const = 0;
+    virtual std::string get_name() const = 0;
     virtual void interact(AbstractProductA* productA) const = 0;
 };
 
 class ConcreteProductB1 : public AbstractProductB {
 public:
-    std::string getName() const override {
+    std::string get_name() const override {
         return "ConcreteProductB1";
     }
 
     void interact(AbstractProductA* productA) const override {
-        std::cout << getName() << " interacts with " << productA->getName() << std::endl;
+        std::cout << get_name() << " interacts with " << productA->getName() << std::endl;
     }
 };
 
 class ConcreteProductB2 : public AbstractProductB {
 public:
-    std::string getName() const override {
+    std::string get_name() const override {
         return "ConcreteProductB2";
     }
 
     void interact(AbstractProductA* productA) const override {
-        std::cout << getName() << " interacts with " << productA->getName() << std::endl;
+        std::cout << get_name() << " interacts with " << productA->getName() << std::endl;
     }
 };
 
 class AbstractFactory {
 public:
     virtual ~AbstractFactory() {}
-    virtual AbstractProductA* createProductA() const = 0;
-    virtual AbstractProductB* createProductB() const = 0;
+    virtual AbstractProductA* create_productA() const = 0;
+    virtual AbstractProductB* create_productB() const = 0;
 };
 
 class ConcreteFactory1 : public AbstractFactory {
 public:
-    AbstractProductA* createProductA() const override {
+    AbstractProductA* create_productA() const override {
         return new ConcreteProductA1();
     }
 
-    AbstractProductB* createProductB() const override {
+    AbstractProductB* create_productB() const override {
         return new ConcreteProductB1();
     }
 };
 
 class ConcreteFactory2 : public AbstractFactory {
 public:
-    AbstractProductA* createProductA() const override {
+    AbstractProductA* create_productA() const override {
         return new ConcreteProductA2();
     }
 
-    AbstractProductB* createProductB() const override {
+    AbstractProductB* create_productB() const override {
         return new ConcreteProductB2();
     }
 };
