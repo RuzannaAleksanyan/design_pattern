e64f5d896d780d455436658bc884aae5c8ccef30
RozAle <110930300+RuzannaAleksanyan@users.noreply.github.com>
2023-09-01 17:38:13 +0400

    Update abstract_factory.cpp

--- creational_patterns/abstract_factory/abstract_factory.cpp
+++ creational_patterns/abstract_factory/abstract_factory.cpp
@@ -3,11 +3,11 @@
 
 int main() {
     AbstractFactory* factory1 = new ConcreteFactory1();
-    AbstractProductA* productA1 = factory1->createProductA();
-    AbstractProductB* productB1 = factory1->createProductB();
+    AbstractProductA* productA1 = factory1->create_productA();
+    AbstractProductB* productB1 = factory1->create_productB();
 
-    std::cout << productA1->getName() << std::endl;
-    std::cout << productB1->getName() << std::endl;
+    std::cout << productA1->get_name() << std::endl;
+    std::cout << productB1->get_name() << std::endl;
     productB1->interact(productA1);
 
     delete productA1;
@@ -15,11 +15,11 @@ int main() {
     delete factory1;
 
     AbstractFactory* factory2 = new ConcreteFactory2();
-    AbstractProductA* productA2 = factory2->createProductA();
-    AbstractProductB* productB2 = factory2->createProductB();
+    AbstractProductA* productA2 = factory2->create_productA();
+    AbstractProductB* productB2 = factory2->create_productB();
 
-    std::cout << productA2->getName() << std::endl;
-    std::cout << productB2->getName() << std::endl;
+    std::cout << productA2->get_name() << std::endl;
+    std::cout << productB2->get_name() << std::endl;
     productB2->interact(productA2);
 
     delete productA2;
