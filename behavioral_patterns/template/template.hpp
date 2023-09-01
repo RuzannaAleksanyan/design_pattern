84a82c586c1e3a76aea15f23e6fc3753fab65600
RozAle <110930300+RuzannaAleksanyan@users.noreply.github.com>
2023-09-01 19:06:43 +0400

    Create template.hpp

---
+++ behavioral_patterns/template/template.hpp
@@ -0,0 +1,58 @@
+#ifndef PROTOTYPE
+#define PROTOTYPE
+
+#include <iostream>
+
+class Report {
+public:
+    void generate_report() {
+        open_file();
+        write_header();
+        write_data();
+        close_file();
+    }
+
+    virtual void open_file() {
+        std::cout << "Opening the file" << std::endl;
+    }
+
+    virtual void write_header() = 0; 
+
+    virtual void write_data() = 0; 
+
+    virtual void close_file() {
+        std::cout << "Closing the file" << std::endl;
+    }
+};
+
+class PDFReport : public Report {
+public:
+    void write_header() override {
+        std::cout << "Writing PDF header" << std::endl;
+    }
+
+    void write_data() override {
+        std::cout << "Writing data in PDF format" << std::endl;
+    }
+};
+
+class CSVReport : public Report {
+public:
+    void open_file() override {
+        std::cout << "Opening a CSV file" << std::endl;
+    }
+
+    void write_header() override {
+        std::cout << "Writing CSV header" << std::endl;
+    }
+
+    void write_data() override {
+        std::cout << "Writing data in CSV format" << std::endl;
+    }
+
+    void close_file() override {
+        std::cout << "Closing a CSV file" << std::endl;
+    }
+};
+
+#endif
